4c7afef9c2f2e157113505507629fe40046efed4
JosileteRodrigues <44982021+JosileteRodrigues@users.noreply.github.com>
2023-09-01 10:46:10 -0300

    Update aluno_service.js

--- servico/aluno_service.js
+++ servico/aluno_service.js
@@ -12,7 +12,20 @@ class AlunoService {
         this.repositorio.inserir(alunoNovo);
         return alunoNovo;
     }
-
+    
+    atualizar(matricula, nome, idade) {
+        const alunoExistente = this.pesquisarPorMatricula(matricula);
+        if (!alunoExistente) {
+            throw new Error('Aluno não encontrado!');
+        }
+        if (idade && idade < 18) {
+            throw new Error("Aluno não pode ser menor de idade.")
+        }
+        alunoExistente.nome = nome;
+        alunoExistente.idade = idade;
+        return alunoExistente;
+    }
+    
     pesquisarPorMatricula(matricula) {
         return this.repositorio.listar().filter(
             aluno => aluno.matricula === matricula);
